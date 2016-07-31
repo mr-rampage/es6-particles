287b949ca8875c07eddef13be34cc81315fb23e7
mr-rampage <readonly@che.wbac.ca>
2016-07-30 21:44:06 -0400

    added pixel renderer

---
+++ src/js/renderer/PixelRenderer.js
@@ -0,0 +1,22 @@
+export default function PixelRenderer(context) {
+  const pixel = context.createImageData(1, 1);
+
+  const setColour = colour =>
+    colour.forEach((value, index) => {
+      pixel.data[index] = value;
+    });
+
+  const getColourAt = location =>
+    context.getImageData(location[0], location[1], 1, 1).data;
+
+  this.draw = function(location, colour) {
+    const originalColour = getColourAt(location).map(value => value);
+    setColour(colour);
+    context.putImageData(pixel, location[0], location[1]);
+
+    return function erase() {
+      setColour(originalColour);
+      context.putImageData(pixel, location[0], location[1]);
+    };
+  };
+}
